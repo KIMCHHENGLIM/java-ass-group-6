bc7f3705c660dd9c3d29730001a8d89ca0911956
LIM KIMCHHENG <goodboy00710@yahoo.com>
2018-07-16 21:11:59 +0700

    16-July-2018

--- AirlineProject/src/main/java/ckcc/assignment/AirlineProject/MainMenu.java
+++ AirlineProject/src/main/java/ckcc/assignment/AirlineProject/MainMenu.java
@@ -1,40 +1,51 @@
 package ckcc.assignment.AirlineProject;
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
-public class MainMenu extends JFrame {
+public class MainMenu extends JFrame implements ActionListener{
 	private JMenuBar menuBar;
 	private JMenu mFile, mAirline, mFlight, mSchedule, mHelp;
 	//For Menu File
 	private JMenuItem mItemExit;
 	//For Menu Airline
-	private JMenuItem mItemAddAirline;
+	private JMenuItem mItemAddAirline, mItemShowAirline;
 	//For Menu Flight
-	private JMenuItem mItemAddFlight;
-	private JMenuItem mItemUpdateFlightStatus;
-	private JMenuItem mItemShowFlight;
+	private JMenuItem mItemAddFlight, mItemUpdateFlightStatus, mItemShowFlight;
 	//For Menu Schedule
-	private JMenuItem mItemDepartures;
-	private JMenuItem mItemArrivals;
-	private JMenuItem mItemClearSchedule;
+	private JMenuItem mItemDepartures, mItemArrivals, mItemClearSchedule;
 	//For Menu Help
-	private JMenuItem mItemWelcome;
-	private JMenuItem mItemHelpContents;
-	private JMenuItem mItemCheckUpdate;
-	private JMenuItem mItemAbout;
+	private JMenuItem mItemWelcome, mItemHelpContents, mItemCheckUpdate, mItemAbout;
+	//JTree 
+	private JTree jTree;
+	//JTab
+	private JTabbedPane jTab;
+	//Add Airline
+	private JTextField txtAirlineName, txtAirlineCode, txtAircraftModel;
+	private JTextField txtFirstClass, txtBusinessClass, txtEconomyClass;
+	private JButton btnSave, btnClear;
+	private JLabel lblResult;
+	
 	public MainMenu() {
 		//Create Object Menu Item of File
 		mItemExit = new JMenuItem("Exit");
 		//Create Object Menu File and add its Item
 		mFile = new JMenu("File");
 		mFile.add(mItemExit);
+		mItemExit.addActionListener(this);
 		//================END MENU FILE==================/
 		//Create Object Menu Item of Airline
 		mItemAddAirline = new JMenuItem("Add Airline");
+		mItemShowAirline = new JMenuItem("Show Airline");
 		//Create Object Menu Airline and add its Item
 		mAirline = new JMenu("Airline");
 		mAirline.add(mItemAddAirline);
+		mAirline.add(mItemShowAirline);
+		mItemAddAirline.addActionListener(this);
 		//================END MENU AIRLINE==================/
 		//Create Object Menu Item of Flight
 		mItemAddFlight = new JMenuItem("Add Flight");
@@ -68,6 +79,7 @@ public class MainMenu extends JFrame {
 		mHelp.add(new JSeparator()); // Add Line to Divide
 		mHelp.add(mItemCheckUpdate);
 		mHelp.add(mItemAbout);
+		mItemWelcome.addActionListener(this);
 		//=========================END MENU HELP ======================/
 		
 		// Create Object Menu Bar and add its menus
@@ -76,21 +88,17 @@ public class MainMenu extends JFrame {
 		menuBar.add(mAirline); //Add Airline
 		menuBar.add(mFlight); //Add Flight
 		menuBar.add(mSchedule); //Add Schedule
-		menuBar.add(mHelp); //Add Help
+		menuBar.add(mHelp); //Add Help		
 		//========================END MENU BAR ======================/
 		
 		//**Start Left And Right Panel
 		//Add JTree into LeftPanel
-		JPanel leftPanel = new JPanel();
-		leftPanel.add(new JLabel("Left"));
-		//leftPanel.add(createJTreePanel());
-		//JTree leftJTree = createJTree();
-		//Add RightPanel
-		JPanel rightPanel = new JPanel();
-		rightPanel.add(new JLabel("Right"));
-		//jTab = new JTabbedPane();
+		JTree leftJTree = createJTree();
+		//Add JTab into RightPanel	
+		jTab = new JTabbedPane();
+		//Split to 2 Panel
 		JSplitPane jsp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true, 
-										leftPanel, rightPanel);		
+								leftJTree, jTab);		
 		jsp.setDividerLocation(180); // To set space between divider 
 		//========================END LEFT AND RIGHT PANEL======================/
 		
@@ -102,5 +110,215 @@ public class MainMenu extends JFrame {
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setVisible(true);
 	}
+	
+	public JTree createJTree() {
+
+		//Create Tree Node Airline
+		DefaultMutableTreeNode nodeAirline = new DefaultMutableTreeNode("Airline");
+		//Create Tree Node Add Airline
+		DefaultMutableTreeNode nodeAddAirline = new DefaultMutableTreeNode("Add Airline");
+		DefaultMutableTreeNode nodeShowAirline = new DefaultMutableTreeNode("Show Airline");
+		nodeAirline.add(nodeAddAirline);
+		nodeAirline.add(nodeShowAirline);
+		//===========================END TREE NODE OF AIRLINE ====================/		
+		//Create Tree Node Flight
+		DefaultMutableTreeNode nodeFlight = new DefaultMutableTreeNode("Flight");
+		//Create Tree Node Add Flight
+		DefaultMutableTreeNode nodeAddFlight = new DefaultMutableTreeNode("Add Flight");
+		DefaultMutableTreeNode nodeUpdateFlight = new DefaultMutableTreeNode("Update Flight Status");
+		DefaultMutableTreeNode nodeShowFlight = new DefaultMutableTreeNode("Show Flight Information");
+		nodeFlight.add(nodeAddFlight);
+		nodeFlight.add(nodeUpdateFlight);
+		nodeFlight.add(nodeShowFlight);
+		//===========================END TREE NODE OF FLIGHT ====================/
+		//Create Tree Node Schedule
+		DefaultMutableTreeNode nodeSchedule = new DefaultMutableTreeNode("Schedule");
+		//Create Tree Node Add Schedule
+		DefaultMutableTreeNode nodeDepartures = new DefaultMutableTreeNode("Departures");
+		DefaultMutableTreeNode nodeArrivals = new DefaultMutableTreeNode("Arrivals");
+		DefaultMutableTreeNode nodeClearSchedule = new DefaultMutableTreeNode("Clear Schedule");
+		nodeSchedule.add(nodeDepartures);
+		nodeSchedule.add(nodeArrivals);
+		nodeSchedule.add(nodeClearSchedule);
+		//===========================END TREE NODE OF SCHEDULE ====================/
+		
+		//**Add to root	
+		//Create Tree Root Node
+		DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("Root Node");
+		//Add node to root	
+		rootNode.add(nodeAirline);
+		rootNode.add(nodeFlight);
+		rootNode.add(nodeSchedule);
+		//==========================END ADD TO ROOT NODE ============================/
+		
+		//Create Object of JTree 
+		jTree = new JTree(rootNode);
+		jTree.setRowHeight(20);
+		jTree.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		jTree.setRootVisible(false);
+		//==========================END CREATE OBJECT JTREE==========================/
+		
+		//Expand all tree nodes
+		for(int i=0; i<=jTree.getRowCount(); i++)
+			jTree.expandRow(i); //Expand root node
+		//Add Mouse Listener To Tree
+		jTree.addMouseListener(new MouseAdapter() { //Double click event		
+			public void mousePressed(MouseEvent e) {
+				//Find Selected Node of Trees
+				int selectedNode = jTree.getRowForLocation(e.getX(), e.getY());//It's mean Have we selected on any tree node?
+				// Condition when mouse pressed on a specific node
+				if(selectedNode != -1) { //If select == -1 means it didn't select on any Node
+										//If select != -1 means it selected on any node 
+					// When mouse pressed is double clicked
+					if(e.getClickCount() == 2) {
+						//Get for tree path
+						TreePath treePath = jTree.getPathForLocation(e.getX(), e.getY());
+											//It's result [Root Node, Airline, Add Airline] When We Code System.out.println(treePath);
+						//Get last selected tree path
+						String lastSelectedPath = treePath.getLastPathComponent().toString();
+											//We use lastSelectedPath to get Last element of Array treePath
+						if(lastSelectedPath.equals("Add Airline")) {
+							performOpenAddAirline();
+						}
+						else if(lastSelectedPath.equals("Show Airline")) {
+							
+						}
+						else if(lastSelectedPath.equals("Add Flight")) {
+							
+						}
+						else if(lastSelectedPath.equals("Update Flight Status")) {
+							
+						}
+						else if(lastSelectedPath.equals("Show Flight Information")) {
+							
+						}
+						else if(lastSelectedPath.equals("Departures")) {
+							
+						}
+						else if(lastSelectedPath.equals("Arrivals")) {
+							
+						}
+						else if(lastSelectedPath.equals("Clear Schedule")) {
+							
+						}
+					}
+				}
+			}
+		});
+		//=======================END ADD MOUSELISTENER EVENT=====================/
+		return jTree;
+	}
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == mItemExit) {
+			dispose();
+		}
+		else if(e.getSource() == mItemAddAirline) {
+			performOpenAddAirline();			
+		}
+		else if(e.getSource() == mItemShowAirline) {
+					
+		}
+		else if(e.getSource() == mItemAddFlight) {
+			
+		}
+		else if(e.getSource() == mItemUpdateFlightStatus) {
+					
+		}
+		else if(e.getSource() == mItemShowFlight) {
+			
+		}
+		else if(e.getSource() == mItemDepartures) {
+			
+		}
+		else if(e.getSource() == mItemArrivals) {
+			
+		}
+		else if(e.getSource() == mItemClearSchedule) {
+					
+		}
+		else if(e.getSource() == mItemWelcome) {
+			performOpenWelcome();
+		}
+		else if(e.getSource() == mItemHelpContents) {
+			
+		}
+		else if(e.getSource() == mItemCheckUpdate) {
+			
+		}
+		else if(e.getSource() == mItemAbout) {
+			
+		}
+	}
+	private void performOpenWelcome() {
+		JLabel lblPhoto = new JLabel("", SwingConstants.CENTER);
+		ImageIcon icon = new ImageIcon("images/Airline-Logo-NoBackground.png");
+		lblPhoto.setIcon(icon);
+		
+		JPanel panelPhoto = new JPanel(new BorderLayout(10,10));
+		panelPhoto.add(lblPhoto, BorderLayout.CENTER);
+		
+		jTab.addTab("Welcome", panelPhoto);
+		jTab.setSelectedComponent(panelPhoto);
+	}
+	private void performOpenAddAirline() {
+		JPanel addAirlinePanel = new JPanel();//new BorderLayout(10, 10));
+		// Create Group Box - Add Airline
+		TitledBorder tBorderNewEmp = BorderFactory.createTitledBorder("ADD AIRLINE");
+		tBorderNewEmp.setTitleJustification(TitledBorder.CENTER);
+		addAirlinePanel.setBorder(tBorderNewEmp);
+		
+		//Block AddAirline - Airline Info
+		JPanel airlineInfo = new JPanel(new GridLayout(6, 2, 10, 10));
+		airlineInfo.add(new Label("Airline Name"));
+		airlineInfo.add(txtAirlineName = new JTextField());
+		airlineInfo.add(new Label("Airline Code"));
+		airlineInfo.add(txtAirlineCode = new JTextField());
+		airlineInfo.add(new Label("Aircraft model"));
+		airlineInfo.add(txtAircraftModel = new JTextField());
+		airlineInfo.add(new Label("First Class Capacity"));
+		airlineInfo.add(txtFirstClass = new JTextField());
+		airlineInfo.add(new Label("Business Class Capacity"));
+		airlineInfo.add(txtBusinessClass = new JTextField());
+		airlineInfo.add(new Label("Economy Class Capacity"));
+		airlineInfo.add(txtEconomyClass = new JTextField());
+		//Create Block AddAirline - Airline Info - Final
+		JPanel airlineInfo_Final = new JPanel(new BorderLayout(10,10));
+		airlineInfo_Final.add(new JLabel("Add Airline Information"), BorderLayout.NORTH);
+		airlineInfo_Final.add(new JSeparator(), BorderLayout.CENTER);
+		airlineInfo_Final.add(airlineInfo,BorderLayout.SOUTH);
+		//====================END BLOCK ADDAIRLINE INFO==============================/
+		//Create AddAirline - Action Button with Action Event
+		JPanel airlineButtonPanel = new JPanel(new FlowLayout());
+		airlineButtonPanel.add(btnSave = new JButton("Save"));
+		airlineButtonPanel.add(btnClear = new JButton("Clear"));
+		btnSave.addActionListener(this);
+		btnClear.addActionListener(this);
+		//Create AddAirline - Action Button - Final
+		JPanel airlineButtonPanel_Final = new JPanel(new BorderLayout(10, 10));
+		airlineButtonPanel_Final.add(airlineInfo_Final, BorderLayout.NORTH);
+		airlineButtonPanel_Final.add(new JSeparator(), BorderLayout.CENTER);
+		airlineButtonPanel_Final.add(airlineButtonPanel, BorderLayout.SOUTH);
+		//====================END BLOCK ADDAIRLINE ACTION BUTTON==============================/
+		//Create Block AddAirline Result
+		JPanel airlineResultPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		airlineResultPanel.add(lblResult = new JLabel("Fill Information, Please!"));
+		//Create Block AddAirline Result - Final
+		JPanel airlineResultPanel_Final = new JPanel(new BorderLayout(10, 10));
+		airlineResultPanel_Final.add(airlineButtonPanel_Final, BorderLayout.NORTH);
+		airlineResultPanel_Final.add(airlineResultPanel, BorderLayout.CENTER);
+		//====================END BLOCK ADDAIRLINE RESULT==============================/
+		
+		addAirlinePanel.add(airlineResultPanel_Final);
+		
+		jTab.addTab("Add Airline", addAirlinePanel);
+		jTab.setSelectedComponent(addAirlinePanel);
+	}
+
+	public static void main(String[] args) {
+		//Call MainMenu
+		MainMenu main = new MainMenu();
+	}
+
 	
 }
